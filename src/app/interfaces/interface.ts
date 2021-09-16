16a70fa587a58541d6c4b65c5ac18aeb5e2aba1c
Rikesh <recasechhunju@gmail.com>
2021-09-16 20:56:40 +0545

    Update interfaces

--- src/app/interfaces/interface.ts
+++ src/app/interfaces/interface.ts
@@ -2,8 +2,17 @@ export interface Cell {
   notes: number[];
   value: number;
   changeable: boolean;
+  highlight: boolean;
+  selected: boolean;
+  error?: boolean;
+  selectedValue?: boolean;
 }
 export interface CellIndex {
   rowIndex: number;
   columnIndex: number;
+}
+
+export interface CellHistory {
+  index: CellIndex;
+  cell: Cell;
 }
