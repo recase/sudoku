4f5558fe527dcf0220abbde6a18fef578bc172ee
Rikesh <recasechhunju@gmail.com>
2021-09-16 13:01:06 +0545

    Replace static data with actual sudoku data

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cell } from '../interfaces/interface';
+import { SudokuService } from '../services/sudoku.service';
 
 @Component({
   selector: 'app-main',
@@ -6,19 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  public sudoku = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [9, 8, 7, 6, 5, 4, 3, 2, 1],
-  ];
+  // public sudoku = [
+  //   [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  //   [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  //   [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  //   [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  //   [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  //   [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  //   [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  //   [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  //   [9, 8, 7, 6, 5, 4, 3, 2, 1],
+  // ];
+  public sudoku: Cell[][];
 
-  constructor() {}
+  public num_buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  ngOnInit(): void {}
+  constructor(private sudokuService: SudokuService) {
+    this.sudoku = this.sudokuService.generateSudoku();
+  }
+
+  ngOnInit(): void {
+    console.log(this.sudoku);
+  }
 }
