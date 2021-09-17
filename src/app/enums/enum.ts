1521fa9f9c1cf2c9ce4f63ffc332926e6c5707b3
Rikesh <recasechhunju@gmail.com>
2021-09-17 15:34:04 +0545

    Add enum types

---
+++ src/app/enums/enum.ts
@@ -0,0 +1,20 @@
+export enum LevelTypes {
+  Easy = 'easy',
+  Difficult = 'difficult',
+  TimeChallenge = 'time challenge',
+}
+
+export enum emptyBlock {
+  Easy = 36,
+  Difficult = 45,
+  TimeChallenge = 45,
+}
+
+export enum StartCheck {
+  EasyOne = 1500,
+  EasyTwo = 600,
+  EasyThree = 300,
+  DifficultOne = 1500,
+  DiffcultTwo = 720,
+  DifficultThree = 360,
+}
