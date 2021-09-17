5b68dd8cf1439cf64545413d2e9b5dddf000c71c
Rikesh <recasechhunju@gmail.com>
2021-09-17 15:37:53 +0545

    Update datatype and conditions on service

--- src/app/services/time-to-string.service.ts
+++ src/app/services/time-to-string.service.ts
@@ -6,23 +6,27 @@ import { Injectable } from '@angular/core';
 export class TimeToStringService {
   constructor() {}
 
-  public getDisplayTimer(time: number): string {
-    const hours = '0' + Math.floor(time / 3600);
-    const minutes = '0' + Math.floor((time % 3600) / 60);
-    const seconds = '0' + Math.floor((time % 3600) % 60);
+  public getDisplayTimer(time: number | null): string {
+    if (time) {
+      const hours = '0' + Math.floor(time / 3600);
+      const minutes = '0' + Math.floor((time % 3600) / 60);
+      const seconds = '0' + Math.floor((time % 3600) % 60);
 
-    let hourValue = null;
-    if (hours !== '00') {
-      hourValue = `${hours.slice(-2, -1)}${hours.slice(-1)}:`;
+      let hourValue = null;
+      if (hours !== '00') {
+        hourValue = `${hours.slice(-2, -1)}${hours.slice(-1)}:`;
+      }
+
+      const stringValue = `${minutes.slice(-2, -1)}${minutes.slice(
+        -1
+      )}:${seconds.slice(-2, -1)}${seconds.slice(-1)}s`;
+
+      if (hourValue) {
+        return hourValue + stringValue;
+      }
+      return stringValue;
+    } else {
+      return '';
     }
-
-    const stringValue = `${minutes.slice(-2, -1)}${minutes.slice(
-      -1
-    )}:${seconds.slice(-2, -1)}${seconds.slice(-1)}s`;
-
-    if (hourValue) {
-      return hourValue + stringValue;
-    }
-    return stringValue;
   }
 }
